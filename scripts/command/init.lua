f82dabcc7dec3a54cf28223dc79a380601e2ac08
actboy168 <actboy168@gmail.com>
2019-01-14 13:37:27 +0800

    修正一个错误提示

--- scripts/command/init.lua
+++ scripts/command/init.lua
@@ -1,12 +1,12 @@
 local lm = require 'luamake'
 
 local sandbox = require "sandbox"
-sandbox(WORKDIR:string(), ARGUMENTS.f or 'make.lua', function(name, mode)
+assert(sandbox(WORKDIR:string(), ARGUMENTS.f or 'make.lua', function(name, mode)
     local f, err = io.open(name, mode)
     if f then
         lm:add_script(name)
     end
     return f, err
-end, { luamake = lm:export() })(table.unpack(arg))
+end, { luamake = lm:export() }))(table.unpack(arg))
 
 lm:finish()

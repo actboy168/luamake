140c65d2f79e48f2315876cf60831484133ba21a
actboy168 <actboy168@gmail.com>
2019-01-15 11:49:49 +0800

    remake

--- scripts/command/make.lua
+++ scripts/command/make.lua
@@ -4,7 +4,7 @@ local util = require 'util'
 local build_ninja = util.script()
 
 if not fs.exists(build_ninja) then
-    util.command('init')
+    util.command 'init'
 end
 
 util.ninja {}
---
+++ scripts/command/remake.lua
@@ -0,0 +1,3 @@
+local util = require 'util'
+util.command 'init'
+util.ninja {}

3b23edafca78911c1bf5720468bbca8293d27583
actboy168 <actboy168@gmail.com>
2019-02-11 14:42:44 +0800

    更新bee

--- make.lua
+++ make.lua
@@ -19,15 +19,15 @@ if lm.plat == 'msvc' then
         pool = "console",
     }
     lm:build "copy_bee_1" {
-        "cmd.exe", "/C", "copy", "/Y", "3rd\\bee.lua\\bin\\msvc_x86_release\\bootstrap.exe", "luamake.exe",
+        "cmd.exe", "/C", "copy", "/Y", "3rd\\bee.lua\\build\\msvc\\bin\\bootstrap.exe", "luamake.exe",
         deps = "bee"
     }
     lm:build "copy_bee_2" {
-        "cmd.exe", "/C", "copy", "/Y", "3rd\\bee.lua\\bin\\msvc_x86_release\\bee.dll", "bee.dll",
+        "cmd.exe", "/C", "copy", "/Y", "3rd\\bee.lua\\build\\msvc\\bin\\bee.dll", "bee.dll",
         deps = "bee"
     }
     lm:build "copy_bee_3" {
-        "cmd.exe", "/C", "copy", "/Y", "3rd\\bee.lua\\bin\\msvc_x86_release\\lua54.dll", "lua54.dll",
+        "cmd.exe", "/C", "copy", "/Y", "3rd\\bee.lua\\build\\msvc\\bin\\lua54.dll", "lua54.dll",
         deps = "bee"
     }
 else
@@ -41,16 +41,16 @@ else
     local dll = (lm.plat == 'mingw') and ".dll" or ".so"
 
     lm:build "copy_bee_1" {
-       "cp", "3rd/bee.lua/bin/"..lm.plat.."_release/bootstrap"..exe, "luamake"..exe,
+       "cp", "3rd/bee.lua/build/"..lm.plat.."/bin/bootstrap"..exe, "luamake"..exe,
         deps = "bee"
     }
     lm:build "copy_bee_2" {
-        "cp", "3rd/bee.lua/bin/"..lm.plat.."_release/bee"..dll, "bee"..dll,
+        "cp", "3rd/bee.lua/build/"..lm.plat.."/bin/bee"..dll, "bee"..dll,
         deps = "bee"
     }
     if lm.plat == 'mingw' then
         lm:build "copy_bee_3" {
-            "cp", "3rd/bee.lua/bin/"..lm.plat.."_release/lua54"..dll, "lua54"..dll,
+            "cp", "3rd/bee.lua/build/"..lm.plat.."/bin/lua54"..dll, "lua54"..dll,
             deps = "bee"
         }
     end

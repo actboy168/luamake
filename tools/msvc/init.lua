f783b64ab70768c050446c263c7a828c9d5e58a8
actboy168 <actboy168@gmail.com>
2020-07-31 15:15:14 +0800

    简化代码

--- tools/msvc/init.lua
+++ tools/msvc/init.lua
@@ -1,51 +1,19 @@
-local output = ...
+os.execute "mkdir temp 2>nul"
+os.execute "type nul>temp/test.h"
+os.execute "echo #include \"test.h\" >temp/test.c"
 
-require 'bee'
-local fs = require 'bee.filesystem'
-local sp = require 'bee.subprocess'
-
-local function createfile(filename, content)
-    local f = assert(io.open(filename:string(), 'w'))
-    if content then
-        f:write(content)
-    end
-    f:close()
-end
-
-local testdir = fs.path 'temp'
-fs.create_directories(testdir)
-createfile(testdir / 'test.h')
-createfile(testdir / 'test.c', '#include "test.h"')
-local process = assert(sp.shell {
-    'cl', '/showIncludes', '/nologo', '-c', 'test.c',
-    cwd = testdir,
-    stdout = true,
-    stderr = true,
-})
+local stdout = io.popen("cd temp && cl /showIncludes /nologo test.c")
 local prefix
-for line in process.stdout:lines() do
+for line in stdout:lines() do
     local m = line:match('[^:]+:[^:]+:')
     if m then
         prefix = m
         break
     end
 end
-process.stdout:close()
-process.stderr:close()
-process:wait()
-fs.remove_all(testdir)
+stdout:close()
+os.execute "rmdir /q /s temp"
 assert(prefix, "can't find msvc.")
 
-fs.create_directories(fs.path(output):parent_path())
-
-local template = [[
-builddir = build/msvc
-msvc_deps_prefix = %s
-subninja ninja/msvc.ninja
-]]
-
-assert(
-    assert(
-        io.open(output, 'wb')
-    ):write(template:format(prefix))
-):close()
+os.execute [[mkdir ..\..\3rd\bee.lua\build\msvc 2>nul]]
+os.execute(([[cd ..\..\3rd\bee.lua\build\msvc && (echo builddir = build/msvc&&echo msvc_deps_prefix = %s&&echo subninja ninja/msvc.ninja) >msvc-init.ninja]]):format(prefix))

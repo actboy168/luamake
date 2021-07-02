766d4dcfae430549e020724e482c1ab5413dd490
actboy168 <actboy168@gmail.com>
2021-07-02 17:45:41 +0800

    移除refreshenv

--- compile/msvc/setpath.bat
+++ compile/msvc/setpath.bat
@@ -2,5 +2,4 @@ SET setpath=%cd%
 echo %path%|findstr /i %setpath% 1>nul && (goto END) 
 set "path=%setpath%;%path%"
 reg add "HKEY_CURRENT_USER\Environment" /v "Path" /t REG_EXPAND_SZ /d "%PATH%" /f 1>nul
-refreshenv
 :END

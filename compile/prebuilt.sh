a1fa507a60865bdabfca0012fb9620b150c46f2f
actboy168 <actboy168@gmail.com>
2021-08-09 22:37:39 +0800

    android构建脚本

--- compile/prebuilt.sh
+++ compile/prebuilt.sh
@@ -2,8 +2,10 @@ luamake init -prebuilt -builddir build/msvc  -hostos windows
 luamake init -prebuilt -builddir build/mingw -hostos windows -compiler gcc
 luamake init -prebuilt -builddir build/linux -hostos linux
 luamake init -prebuilt -builddir build/macos -hostos macos
+luamake init -prebuilt -builddir build/android -hostos android
 
 cp -afv build/msvc/build.ninja  compile/ninja/msvc.ninja
 cp -afv build/mingw/build.ninja compile/ninja/mingw.ninja
 cp -afv build/linux/build.ninja compile/ninja/linux.ninja
 cp -afv build/macos/build.ninja compile/ninja/macos.ninja
+cp -afv build/android/build.ninja compile/ninja/android.ninja

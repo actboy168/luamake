c8e4cda86b5dffd7d1b3f1e497f8a96f57e10fc7
actboy168 <actboy168@gmail.com>
2021-08-19 01:01:42 +0800

    fixes bug

--- compile/msvc/setpath.bat
+++ compile/msvc/setpath.bat
@@ -1,5 +1,11 @@
 SET setpath=%cd%
-echo %path%|findstr /i %setpath% 1>nul && (goto END) 
-set "path=%setpath%;%path%"
-reg add "HKEY_CURRENT_USER\Environment" /v "Path" /t REG_EXPAND_SZ /d "%PATH%" /f 1>nul
+echo %path%|findstr /i %setpath% 1>nul && (goto END)
+for /F "tokens=1,2*" %%i in ('reg query "HKCU\Environment" /v "Path"') do (
+    if "%%i"=="Path" (
+        set regpath=%%~k
+    )
+) 
+set "path=%setpath%;%path%" 
+set "regpath=%setpath%;%regpath%"
+reg add "HKCU\Environment" /v "Path" /t REG_EXPAND_SZ /d "%regpath%" /f 1>nul
 :END

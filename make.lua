4e2736cb344384d11ae66ba90ea0e657ac65a098
actboy168 <actboy168@gmail.com>
2021-09-06 23:45:27 +0800

    更新bee

--- make.lua
+++ make.lua
@@ -4,29 +4,16 @@ local isWindows = lm.os == 'windows'
 local exe = isWindows and ".exe" or ""
 local dll = isWindows and ".dll" or ".so"
 
+lm.EXE_NAME = "luamake"
 lm:import "3rd/bee.lua/make.lua"
 
-lm:copy "copy_bootstrap" {
-    input = "$bin/bootstrap"..exe,
+lm:copy "copy_luamake" {
+    input = "$bin/luamake"..exe,
     output = "luamake"..exe,
-    deps = "bootstrap",
+    deps = "luamake",
 }
-lm:copy "copy_bee" {
-    input = "$bin/bee"..dll,
-    output = "bee"..dll,
-    deps = "bee"
-}
-if isWindows then
-    lm:copy "copy_lua54" {
-        input = "$bin/lua54"..dll,
-        output = "lua54"..dll,
-        deps = "lua54"
-    }
-end
 
 lm:default {
     "test",
-    "copy_bootstrap",
-    "copy_bee",
-    isWindows and "copy_lua54",
+    "copy_luamake",
 }

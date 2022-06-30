647e7dc365d74ca09b57cd235f88973579c02006
actboy168 <actboy168@gmail.com>
2022-06-30 10:01:26 +0800

    support netbsd #24

--- compile/prebuilt.sh
+++ compile/prebuilt.sh
@@ -3,9 +3,11 @@ luamake init -prebuilt -builddir build/mingw -hostos windows -hostshell sh
 luamake init -prebuilt -builddir build/linux -hostos linux
 luamake init -prebuilt -builddir build/macos -hostos macos
 luamake init -prebuilt -builddir build/android -hostos android
+luamake init -prebuilt -builddir build/netbsd -hostos netbsd
 
 cp -afv build/msvc/build.ninja  compile/ninja/msvc.ninja
 cp -afv build/mingw/build.ninja compile/ninja/mingw.ninja
 cp -afv build/linux/build.ninja compile/ninja/linux.ninja
 cp -afv build/macos/build.ninja compile/ninja/macos.ninja
 cp -afv build/android/build.ninja compile/ninja/android.ninja
+cp -afv build/netbsd/build.ninja compile/ninja/netbsd.ninja
--- scripts/globals.lua
+++ scripts/globals.lua
@@ -1,4 +1,3 @@
-local OS = require "bee.platform".os
 local arguments = require "arguments"
 
 local globals = {}
@@ -10,7 +9,7 @@ end
 globals.mode = globals.mode or "release"
 globals.crt = globals.crt or "dynamic"
 
-globals.hostos = globals.hostos or OS
+globals.hostos = globals.hostos or require "bee.platform".os
 globals.hostshell = globals.hostshell or (function()
     if globals.hostos == "windows" then
         if os.getenv "MSYSTEM" then

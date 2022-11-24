3f37edc364ba92dd92d3f4569a6b53bea0f4907e
actboy168 <actboy168@gmail.com>
2022-11-24 10:42:44 +0800

    add build.bat

---
+++ compile/build.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+CALL compile\msvc\find_msvc.bat
+CALL compile\msvc\generate_msvc_deps_prefix.bat
+@ECHO ON
+
+ninja -f build\msvc\compile.ninja
--- compile/install.bat
+++ compile/install.bat
@@ -1,10 +1,4 @@
+@CALL compile\build.bat
 @ECHO OFF
-CALL compile\msvc\find_msvc.bat
-call compile\msvc\generate_msvc_deps_prefix.bat
-@ECHO ON
-
-ninja -f build\msvc\compile.ninja
-
-@ECHO OFF
-call compile\msvc\setpath.bat
+CALL compile\msvc\setpath.bat
 @ECHO ON

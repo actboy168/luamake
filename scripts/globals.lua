1c89f9d12d58e68693b4abb62ffd59ab71fe172b
actboy168 <actboy168@gmail.com>
2023-03-29 22:52:22 +0800

    android use clang

--- scripts/globals.lua
+++ scripts/globals.lua
@@ -22,10 +22,10 @@ end)()
 globals.compiler = globals.compiler or (function()
     if globals.hostshell == "cmd" then
         return "msvc"
-    elseif globals.hostos == "macos" or globals.hostos == "freebsd" or globals.hostos == "openbsd" or globals.hostos == "netbsd" then
-        return "clang"
+    elseif globals.hostos == "linux" or globals.hostos == "windows" then
+        return "gcc"
     end
-    return "gcc"
+    return "clang"
 end)()
 globals.os = globals.os or (function ()
     if globals.compiler == "emcc" then

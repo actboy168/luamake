7bc16c529953ee9e9b94256f6fc254e22501b9fb
actboy168 <actboy168@gmail.com>
2023-05-05 22:15:01 +0800

    update emcc

--- scripts/compiler/emcc.lua
+++ scripts/compiler/emcc.lua
@@ -1,6 +1,36 @@
 local emcc = require "compiler.clang"
 
-function emcc.update_ldflags()
+function emcc.update_flags(flags, _, cxxflags, attribute)
+    if attribute.lto ~= "off" then
+        if attribute.lto == "thin" then
+            flags[#flags+1] = "-flto=thin"
+        else
+            flags[#flags+1] = "-flto"
+        end
+    end
+    if attribute.rtti == "off" then
+        cxxflags[#cxxflags+1] = "-fno-rtti"
+    end
+    if attribute.mode == "release" then
+        flags[#flags+1] = "-g0"
+    else
+        flags[#flags+1] = "-g3"
+    end
+end
+
+function emcc.update_ldflags(ldflags, attribute)
+    if attribute.lto ~= "off" then
+        if attribute.lto == "thin" then
+            ldflags[#ldflags+1] = "-flto=thin"
+        else
+            ldflags[#ldflags+1] = "-flto"
+        end
+    end
+    if attribute.mode == "release" then
+        ldflags[#ldflags+1] = "-g0"
+    else
+        ldflags[#ldflags+1] = "-g3"
+    end
 end
 
 function emcc.rule_dll(w, name, ldflags)

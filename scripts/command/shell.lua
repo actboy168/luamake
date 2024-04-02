5184efdd9ea7d3ea27e9ec2cb72e07918057ae48
actboy168 <actboy168@gmail.com>
2024-04-02 14:03:26 +0800

    fixes bug

--- scripts/command/shell.lua
+++ scripts/command/shell.lua
@@ -1,7 +1,10 @@
 local action = require "action"
 
 action.init()
-local code = action.execute { table.unpack(arg, 2) }
+local code = action.execute {
+    stdout = io.stdout,
+    table.unpack(arg, 2)
+}
 if code ~= 0 then
     os.exit(code)
 end

8fea6bee28a67e2948da6a9f5ea5c9fe549b565b
actboy168 <actboy168@gmail.com>
2024-04-26 23:39:08 +0800

    fixes bug

--- scripts/compiler/gcc.lua
+++ scripts/compiler/gcc.lua
@@ -76,30 +76,6 @@ function gcc.rule_exe(w, name, ldflags)
     end
 end
 
-function gcc.rule_lib(w, name)
-    if globals.os == "windows" and globals.hostshell == "sh" then
-        -- mingw
-        w:rule("link_"..name, [[sh -c "rm -f $out && $ar rcs $out @$out.rsp"]],
-            {
-                description = "Link    Lib $out",
-                rspfile = "$out.rsp",
-                rspfile_content = "$in",
-            })
-    elseif globals.hostshell == "cmd" then
-        w:rule("link_"..name, [[cmd /c $ar rcs $out @$out.rsp]],
-            {
-                description = "Link    Lib $out",
-                rspfile = "$out.rsp",
-                rspfile_content = "$in_newline",
-            })
-    else
-        w:rule("link_"..name, [[rm -f $out && $ar rcs $out $in]],
-            {
-                description = "Link    Lib $out"
-            })
-    end
-end
-
 -- mingw only
 function gcc.rule_rc(w, name)
     if globals.os == "windows" and globals.hostshell == "sh" then
--- scripts/compiler/gcc_opt.lua
+++ scripts/compiler/gcc_opt.lua
@@ -1,3 +1,5 @@
+local globals = require "globals"
+
 local function format_path(path)
     if path:match " " then
         return '"'..path..'"'
@@ -95,6 +97,30 @@ function gcc.rule_cxx(w, name, flags, cxxflags)
             deps = "gcc",
             depfile = "$out.d"
         })
+end
+
+function gcc.rule_lib(w, name)
+    if globals.os == "windows" and globals.hostshell == "sh" then
+        -- mingw
+        w:rule("link_"..name, [[sh -c "rm -f $out && $ar rcs $out @$out.rsp"]],
+            {
+                description = "Link    Lib $out",
+                rspfile = "$out.rsp",
+                rspfile_content = "$in",
+            })
+    elseif globals.hostshell == "cmd" then
+        w:rule("link_"..name, [[cmd /c $ar rcs $out @$out.rsp]],
+            {
+                description = "Link    Lib $out",
+                rspfile = "$out.rsp",
+                rspfile_content = "$in_newline",
+            })
+    else
+        w:rule("link_"..name, [[rm -f $out && $ar rcs $out $in]],
+            {
+                description = "Link    Lib $out"
+            })
+    end
 end
 
 return gcc

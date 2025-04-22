0c69cab1fd515ca0d7033796029f56fe3edea178
Henry <cootshk@duck.com>
2025-04-22 00:37:49 -0500

    Update install.sh
    - Move cd to above the build.sh path check
    - Add quotes around the alias so it works properly in paths containing spaces

--- compile/install.sh
+++ compile/install.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env sh
 
+DIR="$(cd "$(dirname "$0")"/.. || exit 1; pwd)"
+
+cd "$DIR" || exit 1
+
 if ! sh ./compile/build.sh $*
 then
   exit 1
 fi
 
-DIR="$(cd "$(dirname "$0")"/.. || exit 1; pwd)"
 
 write_alias() {
   if grep -sq "luamake" "$1"
   then
     echo "luamake alias already defined in $1"
   else
-    printf '\nalias luamake="%s"\n' "$DIR/luamake" >> "$1"
+    printf '\nalias luamake="%s"\n' "'$DIR/luamake'" >> "$1"
     echo "luamake alias added to $1. (You may need to restart your shell.)"
   fi
 }

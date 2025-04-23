5dbf46c10ed6e38049f538ee3d7b8d14341ed41e
actboy168 <actboy168@gmail.com>
2025-04-23 10:15:18 +0800

    识别arm64

--- compile/msvc/find_msvc.bat
+++ compile/msvc/find_msvc.bat
@@ -1,12 +1,19 @@
 IF NOT "%VCToolsVersion%" == "" GOTO QUIT
 
 IF /I "%PROCESSOR_ARCHITECTURE%" == "AMD64" GOTO AMD64
+IF /I "%PROCESSOR_ARCHITECTURE%" == "ARM64" GOTO ARM64
 IF DEFINED PROCESSOR_ARCHITEW6432 GOTO AMD64
+SET HOST_ARCH=x86
 SET ARCH=x86
 GOTO END
 :AMD64
 SET ProgramFiles=%ProgramFiles(x86)%
+SET HOST_ARCH=x64
 SET ARCH=x64
+GOTO END
+:ARM64
+SET HOST_ARCH=x64
+SET ARCH=arm64
 :END
 
 FOR /f "usebackq tokens=*" %%i in (`"%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
@@ -17,5 +24,5 @@ IF "%InstallDir%" == "" echo "Can not find msvc." && exit 1
 CALL compile\msvc\find_toolset.bat
 CALL compile\msvc\find_winsdk.bat
 SET VSCMD_SKIP_SENDTELEMETRY=1
-CALL "%InstallDir%\Common7\Tools\vsdevcmd.bat" -arch=%ARCH% -host_arch=%ARCH% -winsdk=%WindowsSDKVersion% -vcvars_ver=%VCToolsVersion% 1>nul
+CALL "%InstallDir%\Common7\Tools\vsdevcmd.bat" -arch=%ARCH% -host_arch=%HOST_ARCH% -winsdk=%WindowsSDKVersion% -vcvars_ver=%VCToolsVersion% 1>nul
 :QUIT

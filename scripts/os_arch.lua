32a52bb852258224704480159c99a029108db946
actboy168 <actboy168@gmail.com>
2025-04-27 12:07:39 +0800

    完善arm的支持

--- scripts/os_arch.lua
+++ scripts/os_arch.lua
@@ -1,21 +1,36 @@
-local os_name = require "bee.platform".os
+local os = require "bee.platform".os
 
-if os_name == "windows" then
+local function shell(command)
+    local f = assert(io.popen(command, "r"))
+    local r = f:read "l"
+    f:close()
+    return r
+end
+
+if os == "windows" then
     -- https://learn.microsoft.com/en-us/archive/blogs/david.wang/howto-detect-process-bitness
     -- https://learn.microsoft.com/en-us/windows/win32/winprog64/wow64-implementation-details
-    local function env_arch()
-        local PROCESSOR_ARCHITEW6432 = os.getenv "PROCESSOR_ARCHITEW6432"
-        if PROCESSOR_ARCHITEW6432 ~= nil then
+    local function get_env(name)
+        name = "%"..name.."%"
+        local value = shell("echo "..name)
+        if value == name then
+            return ""
+        end
+        return value
+    end
+    local function get_env_arch()
+        local PROCESSOR_ARCHITEW6432 = get_env "PROCESSOR_ARCHITEW6432"
+        if PROCESSOR_ARCHITEW6432 ~= "" then
             return PROCESSOR_ARCHITEW6432
         end
-        return os.getenv "PROCESSOR_ARCHITECTURE"
+        return get_env "PROCESSOR_ARCHITECTURE"
     end
     local ArchMap <const> = {
         AMD64 = "x86_64",
         ARM64 = "arm64",
         x86 = "x86",
     }
-    local arch = ArchMap[env_arch() or ""]
+    local arch = ArchMap[get_env_arch()]
     if not arch then
         error("Cannot detect architecture")
     end

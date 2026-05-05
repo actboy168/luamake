a35ae38a8cc9ef94beeb7ae3ab91061495273a49
actboy168 <actboy168@gmail.com>
2026-05-05 20:47:15 +0800

    luamake test 不再依赖globals.lua

--- scripts/command/test.lua
+++ scripts/command/test.lua
@@ -1,10 +1,3 @@
-local globals = require "globals"
-package.cpath = string.gsub([[${luamake}/?.${extension};./${builddir}/bin/?.${extension}]], "%$%{([^}]*)%}", {
-    luamake = package.procdir,
-    builddir = globals.builddir,
-    extension = package.cpath:match "[/\\]%?%.([a-z]+)",
-})
-
 table.insert(arg, 2, "test.lua")
 
 local command = require "command"
